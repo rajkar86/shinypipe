ed0119f0f980cb434204ea62ae5741b7feed0f88
rajkar86 <karthikeyan.rajendran@gmail.com>
2018-03-08 19:05:40 -0500

    Support date and datetime for plot zoom

--- R/p_plot.R
+++ R/p_plot.R
@@ -5,27 +5,32 @@
 #' shinypipe UI for creating a plot with zoom and pan functions
 #' @param id namespace id (string)
 #' @param height height of the widget
+#' @param brush.direction to control direction argument of brushOpts
 #' @export
-ui.plot <- function(id, height = 400) {
+ui.plot <- function(id, height = 400, brush.direction = "xy") {
   # Create a namespace function using the provided id
   ns <- NS(id)
-  l <- list(
-    fillCol(height = height, flex = c(NA, 1),
-    fixedRow(
-      column(2,tags$div(title = paste("Zoom - zoom on selection (Double-click outside the selected region to reset)",
-                                      sep = "\n"),
-                        checkboxGroupInput(ns("toggles"), NULL, inline = T,
-                                  choiceNames = list("Zoom/Pan"),
-                                  choiceValues = list("zoom"),
-                                  selected = NULL))), # TODO expose this?
-      column(10,span(textOutput(ns("message"), inline = T), style="color:green"))
-    ),
-    plotOutput(ns("plot"),
-               brush    = brushOpts(ns("brush"), clip = F),
-               click    = ns("click"),
-               hover    = ns("hover"),
-               dblclick = ns("dblclick")))
+
+  control <- fixedRow(
+    column(2,tags$div(title = paste("Zoom - zoom on selection (Double-click outside the selected region to reset)",
+                                    sep = "\n"),
+                      checkboxGroupInput(ns("toggles"), NULL, inline = T,
+                                         choiceNames = list("Zoom"),
+                                         choiceValues = list("zoom"),
+                                         selected = NULL))), # TODO expose this?
+    column(10,span(textOutput(ns("message"), inline = T), style="color:green"))
   )
+
+  pObj <- plotOutput(ns("plot"),
+                     brush    = brushOpts(ns("plot_brush"),
+                                          direction = brush.direction,
+                                          clip = F, resetOnNew = T),
+                     click    = ns("click"),
+                     hover    = ns("hover"),
+                     dblclick = ns("dblclick"))
+
+
+  l <- list(fillCol(height = height, flex = c(NA, 1), control, pObj))
   tagList(l)
 }
 
@@ -53,27 +58,48 @@ s.plot <- function(input, output, session, plot, data,
 
   observeEvent(input$toggles, {
     if ("zoom" %in% input$toggles)
-      output$message <- renderText("You can select a region to zoom, and drag it to pan.")
+      output$message <- renderText("Select a region to zoom.")
     else
-      output$message <- renderText("Zoom/Pan mode is disabled.")
+      output$message <- renderText("Zoom mode is disabled.")
   }, ignoreNULL = F)
 
   observeEvent(input$dblclick, {
     val$zoomBrush <- NULL
-    session$resetBrush(input$brush$brushId)
+    print(input$plot_brush)
+    # session$resetBrush(input$plot_brush$brushId)
     output$message <- renderText("")
   })
 
   observeEvent(input$click, {
+    if (is.null(input$plot_brush) && !("zoom" %in% input$toggles))
+      output$message <- renderText("")
   })
 
-  observeEvent(input$brush, {
-    if (!is.null(input$brush) && ("zoom" %in% input$toggles)) {
-      val$zoomBrush <- input$brush
+  observeEvent(input$plot_brush, {
+    if (!is.null(input$plot_brush) && ("zoom" %in% input$toggles)) {
+      val$zoomBrush <- input$plot_brush
+      # session$resetBrush(input$plot_brush$brushId)
     }
     if (!("zoom" %in% input$toggles))
       output$message <- renderText("Click outside the region to deselect.")
   })
+
+  observeEvent(plot(),{
+    val$zoomBrush <- NULL # resetOnNew for the cached zoomBrush
+  })
+
+  observeEvent(data(),{
+    val$zoomBrush <- NULL # resetOnNew for the cached zoomBrush
+  })
+
+  convertLimitForType <- function(lim, datatype) {
+    # origin used here may be exposed, if necessary
+    switch (datatype[[1]],
+      "Date" = as.Date(lim, origin="1970-01-01"),
+      "POSIXct" = as.POSIXct(lim, origin = "1970-01-01"),
+      lim
+    )
+  }
 
   output$plot <- renderPlot({
     p <- ggplot2::ggplot(data = data(), mapping()) + theme_light(base_size = 16) + plot()
@@ -81,22 +107,22 @@ s.plot <- function(input, output, session, plot, data,
     brush <- val$zoomBrush
     if (!is.null(brush)) {
       xlim <- c(brush$xmin, brush$xmax)
-      if (class(data()[,get(toString(brush$mapping$x))]) == "Date")
-        xlim <- as.Date(xlim)
-      p <- p + coord_cartesian(xlim = xlim,
-                               ylim = c(brush$ymin, brush$ymax), expand = T)
-      output$message <- renderText("Drag region to pan. Double click to reset.")
+      ylim <- c(brush$ymin, brush$ymax)
+      xlim <- convertLimitForType(xlim, class(data()[,get(toString(brush$mapping$x))]))
+      ylim <- convertLimitForType(ylim, class(data()[,get(toString(brush$mapping$y))]))
+      p <- p + coord_cartesian(xlim, ylim, expand = F)
+      output$message <- renderText("Double click to reset.")
     }
     p
   })
 
   brushedData <- reactive({
     allRows <- !is.null(selected.colname)
-    if(is.null(input$brush) & allRows) {
+    if(is.null(input$plot_brush) & allRows) {
       dt <- copy(data())
       dt[, eval(selected.colname) := F]
     } else {
-      dt <- brushedPoints(data(), input$brush, allRows = allRows)
+      dt <- brushedPoints(data(), input$plot_brush, allRows = allRows)
       if (allRows)
         setnames(dt, "selected_", selected.colname)
     }
